871a3a0bf53191a4a8e23d44c74cb2c4118d1660
Batuhan <PrintfBatuhan@users.noreply.github.com>
2023-03-28 00:51:45 +0300

    HomePage added

--- containers/home/index.js
+++ containers/home/index.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
+import Movies from '@/mocks/movies'
+import Genres from '@/mocks/genres'
+import FeaturedMovie from '@/components/featured-movie'
+import Categories from '@/components/categories'
+import MoviesSection from '@/components/movies-section'
+
 function HomeContainer() {
-  return <div>HomeContainer</div>
+  return <div>
+    <FeaturedMovie movie={Movies.results[0]} />
+    <Categories categories={Genres.genres.slice(0, 5)}/>
+    <MoviesSection 
+      title='Popular Films' 
+      movies={Movies.results.slice(0, 5)}
+    />
+    <MoviesSection 
+      title='Your Favorites' 
+      movies={Movies.results.slice(7, 13)}
+    />
+  </div>
 }
 
 export default HomeContainer
