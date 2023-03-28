08e1f64609d02b6165b2871b05d7e3e8ba7e7c57
Batuhan <PrintfBatuhan@users.noreply.github.com>
2023-03-28 20:48:17 +0300

    Movie Detail Added

---
+++ app/movie/[id]/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import MovieContainer from '@/containers/movie'
+import Movies from '@/mocks/movies'
+
+function MoviePage({ params }) {
+    const movieDetail = Movies.results.find(
+        (movie) => movie.id.toString() === params.id
+    )
+
+    console.log(params)
+
+  return <MovieContainer movie={movieDetail} />
+}
+
+export default MoviePage
